940d5ca37f68d473d7f26afaa51d003726303198
marcocareddu <saldumaccu@tuta.io>
2023-09-04 12:21:43 +0200

    Added trash page content

--- resources/views/admin/games/trash.blade.php
+++ resources/views/admin/games/trash.blade.php
@@ -1,1 +1,53 @@
+@extends('layouts.app')
 
+@section('title', 'Cestino')
+
+@section('content')
+
+    <div class="container">
+        <h1 class="text-center my-3">Cancellati</h1>
+        <div class="row">
+
+
+            @forelse ($games as $game)
+                <div class="card col-6 m-3" style="width: 18rem;">
+                    <img src="{{ $game->thumb }}" class="card-img-top" alt="Image">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $game->name }}</h5>
+
+                        {{-- Buttons --}}
+                        <div class="d-flex justify-content-evenly">
+
+                            {{-- Delete Button --}}
+                            <form class="form-delete" action="{{ route('admin.games.drop', $game) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">Elimina Definitivamente</button>
+                            </form>
+
+                            {{-- Restore Button --}}
+                            {{-- !! ADD RESTORE ROUTE --}}
+                            <form action="" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button class="btn btn-success">Ripristina</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+
+            @empty
+                <div class="alert alert-success">Non ci sono elementi</div>
+            @endforelse
+
+            <div class="d-flex justify-content-center">
+
+                {{-- All games Button --}}
+                <a href="{{ route('admin.games.index') }}" class="btn btn-primary">Ritorna ai progetti</a>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
